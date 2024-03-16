d2273630eee49b59fe82909cd24807ed548f44b3
xlk <1148038404@qq.com>
2024-03-16 15:15:57 +0800

    chore:  add some vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // root: './', // 默认根路径
+  // base: '/api',
+  // publicDir: 'src',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': 'http://api-driver.marsview.cc'
+    }
+  },
+  plugins: [react()]
 })

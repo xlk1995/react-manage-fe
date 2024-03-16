640191a31bd39bf24b9c17c23ceec84e1ba4c437
xlk <1148038404@qq.com>
2024-03-16 15:40:49 +0800

    feat:  router的基本使用

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # a general background system
+
+## 安装
+
+```
+pnpm add react-router-dom
+```
+
+## 使用
+
+```
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+      <Route path='/test' element={<Test />}></Route>
+    </Routes>
+  </BrowserRouter>s
+```
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+function Test() {
+  return <h2>我是测试啊啊啊啊</h2>
+}
 
 ReactDOM.createRoot(
   document.getElementById('root')!
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+      <Route path='/test' element={<Test />}></Route>
+    </Routes>
+  </BrowserRouter>
 )

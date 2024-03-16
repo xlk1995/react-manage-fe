0e0b90b8622df33de5c1e25bc3ff844e627fac9f
xlk <1148038404@qq.com>
2024-03-16 11:59:06 +0800

    chore: add an test prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,16 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'error', // 禁止使用console
+    'no-unused-vars': 'error', // 禁止定义未使用的变量
+    'no-debugger': 'error', // 禁止使用debugger
+    'no-var': 'error' // 要求使用 let 或 const 而不是 var
+  }
 }

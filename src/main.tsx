487d6936d60313f2be46c53fe963c52951700bc2
xlk <1148038404@qq.com>
2024-03-16 16:11:38 +0800

    feat: 路由跳转的几种方法

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,25 @@ import './index.css'
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Link,
+  Navigate
 } from 'react-router-dom'
 
 function Test() {
-  return <h2>我是测试啊啊啊啊</h2>
+  return (
+    <h2>
+      我是测试啊啊啊啊 <Navigate to='/react'></Navigate>
+    </h2>
+  )
+}
+
+function React() {
+  return (
+    <h1>
+      请好好学习React好吗 <Link to='..'>返回</Link>
+    </h1>
+  )
 }
 
 ReactDOM.createRoot(
@@ -17,7 +31,13 @@ ReactDOM.createRoot(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}></Route>
-      <Route path='/test' element={<Test />}></Route>
+      <Route
+        path='/test'
+        element={<Navigate to='/react' />}
+      ></Route>
+
+      <Route path='/t1' element={<Test />}></Route>
+      <Route path='/react' element={<React />}></Route>
     </Routes>
   </BrowserRouter>
 )
